979b632a6c2a265a5838b5b3d7754e96cc06092f
Erik Gunawan <erik.gunawan44@gmail.com>
2024-12-09 23:34:38 +0700

    Update App.tsx
    update app title

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ function App() {
           <div className="flex justify-between h-16">
             <div className="flex">
               <div className="flex-shrink-0 flex items-center">
-                <h1 className="text-xl font-bold text-gray-900">MedRecord</h1>
+                <h1 className="text-xl font-bold text-gray-900">KlikRM - RSAB Harapan Kita</h1>
               </div>
             </div>
             <div className="flex space-x-4">
