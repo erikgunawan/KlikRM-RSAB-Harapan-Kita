16e8fdc24c207942418ac5e4beb30cf605001759
Erik Gunawan <erik.gunawan44@gmail.com>
2024-12-09 23:27:57 +0700

    Update supabase.ts
    update supabase key

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,8 +1,9 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from '../types/supabase';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+const supabaseUrl = 'https://fdcujcfodivdypvnbvix.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZkY3VqY2ZvZGl2ZHlwdm5idml4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzM3NjAwMzgsImV4cCI6MjA0OTMzNjAzOH0.rRTEKHGafn-zBP8khow2j7f3O0Zhejc3mylHKraqkGM';
 
 if (!supabaseUrl || !supabaseKey) {
   throw new Error('Missing Supabase environment variables');
