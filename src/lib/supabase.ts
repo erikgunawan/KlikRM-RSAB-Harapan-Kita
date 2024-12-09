19d7cf7201ded746758a48c3f31b00ec5d003906
Erik Gunawan <erik.gunawan44@gmail.com>
2024-12-09 23:11:27 +0700

    Update supabase.ts
    updata database

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from '../types/supabase';
 
-const supabaseUrl = 'https://xyzcompanysupabase.supabase.co';
-const supabaseKey = 'your-supabase-anon-key';
+const supabaseUrl = 'https://fdcujcfodivdypvnbvix.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZkY3VqY2ZvZGl2ZHlwdm5idml4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzM3NjAwMzgsImV4cCI6MjA0OTMzNjAzOH0.rRTEKHGafn-zBP8khow2j7f3O0Zhejc3mylHKraqkGM';
 
 export const supabase = createClient<Database>(supabaseUrl, supabaseKey);
